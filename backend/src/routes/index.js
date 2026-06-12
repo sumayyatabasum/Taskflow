aea903becdf697ac4f0a6e5d17c06ca8474a6ac2
Shaik Sumayya <shaiksumayya596@gmail.com>
2026-06-12 16:07:31 +0530

    fix: restore body import from express-validator

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const { body } = require("express-validator");
 const { getDashboardStats } = require("../controllers/dashboardController");
 const {
   getTodos,
